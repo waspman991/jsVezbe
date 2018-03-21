df1a39cc65fb23cdce04560c504cf7ccf3cf4429
Rakita <milos.rakita@ucircuit.com>
2018-03-21 18:05:15 +0100

    initial commit

---
+++ dulekurs/app.js
@@ -0,0 +1,12 @@
+console.log(a);
+b();
+
+// var a= "Hello World!"
+
+function b(){
+
+	console.log('Called b!');
+}
+
+// console.log(a);
+// b();
---
+++ dulekurs/app4700.js
@@ -0,0 +1,33 @@
+
+
+// var a;
+// // var a= "Hello World!";
+// console.log(a);
+
+// if (a === undefined){
+// 	console.log("a is undefined");
+// } else {
+
+// 	console.log("a is defined");
+
+// }
+
+// 	function b(){
+
+// 		console.log(myVar);  //leksicki je funkcija u global environmentu a ne u function a, ako je stvimo u funkciju a, pokupice 2 kao value, a ne 1
+// 	}
+
+// function a(){
+// 	var myVar=2;
+// 	// console.log(myVar);
+// 	b();
+// }
+
+// var myVar = 1;
+// a();
+// // console.log(a);
+// b();
+
+var a = 3 + 4; //plus je operator i sustinski je funkcija
+
+console.log(a);
